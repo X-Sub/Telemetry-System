c5eb6c1e076fdd2668c39316833e96b85b993913
Danilo Diaz Tarasco <danilodt@gmail.com>
2015-04-14 18:40:15 -0430

    Inicialización de Threads y Console Log
    Inicialización de Threads y Console Log

--- Python Data Receiver-Transmitter/Data Receiver-Transmitter.py
+++ Python Data Receiver-Transmitter/Data Receiver-Transmitter.py
@@ -1,6 +1,44 @@
+#!/usr/bin/python
+
 import threading
 import serial
+import logging
 import xSub
 
+#THREADS
 
+def xSub_Servidor():
+	'''Este Thread se encarga de la comunicacion por tcp/ip para transmisión de datos'''
+	print(threading.currentThread().getName())
+	return
+
+def xSub_Serial():
+	"""Este Thread se encarga de la comunicacion por Serial para transmisión de datos"""
+	print(threading.currentThread().getName())
+	return
+
+def xSub_DataLogger():
+	"""Este Thread se encarga de la creación de un archivo .txt con la data recibida"""
+	print(threading.currentThread().getName())
+	return
+
+#Creación de threads para multiproceso
 xSub.info() #Info del Proyecto
+
+#Log del programa
+logging.basicConfig(filename='Logger/logger.txt',level=logging.DEBUG)
+logging.info('Test2') # will not print anything
+
+#Creación de Threads
+Servidor = threading.Thread(name='tServidor', target = xSub_Servidor)
+Serial = threading.Thread(name='tSerial', target = xSub_Serial)
+DataLogger = threading.Thread(name='tDataLogger', target = xSub_DataLogger)
+
+#Inicialización de los Threads
+Servidor.start()
+Serial.start()
+DataLogger.start()
+
+
+
+
--- Python Data Receiver-Transmitter/xSub.py
+++ Python Data Receiver-Transmitter/xSub.py
@@ -1,7 +1,37 @@
+#
+#
+#
+#
+
 def info():
-	infoProyecto = u"El xSub es un proyecto del Grupo de Investigacion y Desarrollo en Mecatronica"
+	infoProyecto = u"El X-Sub es un proyecto del Grupo de Investigacion y Desarrollo en Mecatronica"
 	print(infoProyecto)
 
 version = '0.1'
 
-# End of xSub.py
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
