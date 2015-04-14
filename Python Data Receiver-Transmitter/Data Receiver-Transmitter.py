b7872da3ebbec411ebb488b167a545b3840f5100
Danilo Diaz Tarasco <danilodt@gmail.com>
2015-04-14 12:44:45 -0430

    Creación del Proyecto en Python
    Creación del Proyecto en Python y Paquete xSub.py

---
+++ Python Data Receiver-Transmitter/Data Receiver-Transmitter.py
@@ -0,0 +1,6 @@
+import threading
+import serial
+import xSub
+
+
+xSub.info() #Info del Proyecto
---
+++ Python Data Receiver-Transmitter/xSub.py
@@ -0,0 +1,9 @@
+
+
+def info():
+	infoProyecto = u"El xSub es un proyecto del Grupo de Investigacion y Desarrollo en Mecatronica"
+	print(infoProyecto)
+
+version = '0.1'
+
+# End of xSub.py
