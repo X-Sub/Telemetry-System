f5e2e83ef09222fa8a951439e06e0cbc313c8d06
Danilo Diaz Tarasco <danilodt@gmail.com>
2015-04-15 16:40:47 -0430

    Envío de Trama Auxiliar, Sólo AccelY, H y L

--- Spectrum Analyser/Arduino Test Code Gy-521/Arduino_Test_Code_Gy-521/Arduino_Test_Code_Gy-521.ino
+++ Spectrum Analyser/Arduino Test Code Gy-521/Arduino_Test_Code_Gy-521/Arduino_Test_Code_Gy-521.ino
@@ -3,10 +3,21 @@
 // April 2015
 // Public Domain
 
-#include <Wire.h>
-
+#include<Wire.h>
 const int MPU=0x68;  // I2C address of the MPU-6050
-int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
+//Aceleraciones H-> High, L -> Low
+byte AcXH,AcXL;
+byte AcYH,AcYL;
+byte AcZH,AcZL;
+
+//Velocidades Angulares H-> High, L -> Low
+byte GyXH,GyXL;
+byte GyYH,GyYL;
+byte GyZH,GyZL;
+
+//Temp
+byte TmpH,TmpL;
+
 
 void setup(){
   Wire.begin();
@@ -17,25 +28,38 @@ void setup(){
   Serial.begin(115200);
 }
 
-
 void loop(){
   Wire.beginTransmission(MPU);
   Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
   Wire.endTransmission(false);
   Wire.requestFrom(MPU,14,true);  // request a total of 14 registers
-  AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)     
-  AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
-  AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
-  Tmp=Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
-  GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
-  GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
-  GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
-  Serial.print("AcX = "); Serial.print(AcX);
-  Serial.print(" | AcY = "); Serial.print(AcY);
-  Serial.print(" | AcZ = "); Serial.print(AcZ);
-  Serial.print(" | Tmp = "); Serial.print(Tmp/340.00+36.53);  //equation for temperature in degrees C from datasheet
-  Serial.print(" | GyX = "); Serial.print(GyX);
-  Serial.print(" | GyY = "); Serial.print(GyY);
-  Serial.print(" | GyZ = "); Serial.println(GyZ);
-  //delay(333);
+
+  AcXH = Wire.read();
+  AcXL = Wire.read();
+
+  AcYH = Wire.read();
+  AcYL = Wire.read();
+
+  AcZH = Wire.read();
+  AcZL = Wire.read();
+
+  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+  TmpH = Wire.read();
+  TmpL = Wire.read();
+
+
+ GyXH = Wire.read();
+ GyXL = Wire.read();
+
+ GyYH = Wire.read();
+ GyYL = Wire.read();
+
+ GyZH = Wire.read();
+ GyZL = Wire.read();
+
+  Serial.write(0x00);
+  Serial.write(AcYH);
+  Serial.write(AcYL);
+  Serial.write(0xFF);
+  
 }
