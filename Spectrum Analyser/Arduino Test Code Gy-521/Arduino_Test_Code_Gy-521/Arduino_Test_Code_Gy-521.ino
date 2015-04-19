f717d850d6d31ac037052678bb541b5a1c675981
Danilo Diaz Tarasco <danilodt@gmail.com>
2015-04-19 07:47:32 -0430

    Configuración para rango de aceleración +-4G

--- Spectrum Analyser/Arduino Test Code Gy-521/Arduino_Test_Code_Gy-521/Arduino_Test_Code_Gy-521.ino
+++ Spectrum Analyser/Arduino Test Code Gy-521/Arduino_Test_Code_Gy-521/Arduino_Test_Code_Gy-521.ino
@@ -4,6 +4,14 @@
 // Public Domain
 
 #include<Wire.h>
+#define MPU6050_ACCEL_CONFIG       0x1C   // R/W
+#define MPU6050_D3 3
+#define MPU6050_D4 4
+#define MPU6050_AFS_SEL0   MPU6050_D3
+#define MPU6050_AFS_SEL1   MPU6050_D4
+#define MPU6050_AFS_SEL_0 (0)
+#define MPU6050_AFS_SEL_1 (bit(MPU6050_AFS_SEL0))
+
 const int MPU=0x68;  // I2C address of the MPU-6050
 //Aceleraciones H-> High, L -> Low
 byte AcXH,AcXL;
@@ -19,6 +27,7 @@ byte GyZH,GyZL;
 byte TmpH,TmpL;
 
 
+
 void setup(){
   Wire.begin();
   Wire.beginTransmission(MPU);
@@ -26,6 +35,8 @@ void setup(){
   Wire.write(0);     // set to zero (wakes up the MPU-6050)
   Wire.endTransmission(true);
   Serial.begin(115200);
+  
+  MPU6050_write_reg(MPU6050_ACCEL_CONFIG,MPU6050_AFS_SEL_4G);
 }
 
 void loop(){
@@ -65,4 +76,61 @@ void loop(){
 
   //Serial.write(0xFF);
   delay(20);
+}
+
+
+
+// MPU6050_write
+//
+// This is a common function to write multiple bytes to an I2C device.
+//
+// If only a single register is written,
+// use the function MPU_6050_write_reg().
+//
+// Parameters:
+//   start : Start address, use a define for the register
+//   pData : A pointer to the data to write.
+//   size  : The number of bytes to write.
+//
+// If only a single register is written, a pointer
+// to the data has to be used, and the size is
+// a single byte:
+//   int data = 0;        // the data to write
+//   MPU6050_write (MPU6050_PWR_MGMT_1, &c, 1);
+//
+int MPU6050_write(int start, const uint8_t *pData, int size)
+{
+  int n, error;
+
+  Wire.beginTransmission(MPU);
+  n = Wire.write(start);        // write the start address
+  if (n != 1)
+    return (-20);
+
+  n = Wire.write(pData, size);  // write data bytes
+  if (n != size)
+    return (-21);
+
+  error = Wire.endTransmission(true); // release the I2C-bus
+  if (error != 0)
+    return (error);
+
+  return (0);         // return : no error
+}
+
+
+// MPU6050_write_reg
+//
+// An extra function to write a single register.
+// It is just a wrapper around the MPU_6050_write()
+// function, and it is only a convenient function
+// to make it easier to write a single register.
+//
+int MPU6050_write_reg(int reg, uint8_t data)
+{
+  int error;
+
+  error = MPU6050_write(reg, &data, 1);
+
+  return (error);
 }
